244b32fcf17f66bba5d9792da2170cddd3fe2b61
Sibar Al-ani <sibar.alani@gmail.com>
2025-03-11 11:57:13 +0100

    install cors

--- 06-lecture/ecommerce-api/src/index.ts
+++ 06-lecture/ecommerce-api/src/index.ts
@@ -1,12 +1,14 @@
 import dotenv from "dotenv";
 import express from "express";
 import {connectDB} from "./config/db";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 
 // Middleware
 app.use(express.json())
+app.use(cors())
 
 // Routes
 import productRouter from "./routes/products";
